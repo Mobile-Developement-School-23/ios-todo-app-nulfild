efcf259674143fc1c50ccaa6aeb4c4cabfd53a3e
German Kunin <kunin.german@icloud.com>
2023-07-15 02:27:13 +0300

    Закончил делать дз, приступил к украшениям приложения

---
+++ YandexToDoList/Controllers/ChangeModeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ChangeModeViewController.swift
+//  YandexToDoList
+//
+//  Created by Герман Кунин on 15.07.2023.
+//
+
+import UIKit
+
+class ChangeModeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ YandexToDoList/Views/ChangeMode/ChangeModeView.swift
@@ -0,0 +1,20 @@
+//
+//  ChangeModeView.swift
+//  YandexToDoList
+//
+//  Created by Герман Кунин on 15.07.2023.
+//
+
+import UIKit
+
+class ChangeModeView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
